33c719b7d574d5cea46bf06fd9042a3c18c82551
Elkin Kristan <scyke1@nottingham.edu.cn>
2024-09-03 23:16:24 +0700

    Added 'Confirm' button functionality to add entries to the table

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const addEntryButton = document.getElementById("add-entry-button");
 const addEntryDetails = document.querySelector(".add-entry-details-container")
 const cancelEntry = document.getElementById("cancel-entry-button");
+const confirmEntry = document.getElementById("confirm-entry-button");
+const tableBody = document.getElementById("main-table");
 
 //TODO
 function onAddEntryHover(e) {
@@ -18,6 +20,38 @@ function onCancelClick(e) {
     addEntryDetails.style.display = "none";
 }
 
+// confirm entry and input into the table
+function onConfirmClick(e) {
+    e.preventDefault();
+    const entryTitle = document.getElementById('entry-title').value;
+    const entryRating = document.getElementById('entry-rating').value;
+    if(!entryTitle || !entryRating) {
+        alert('Please fill in Title or Rating!');
+    }
+    else {
+        let today = new Date();
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        const yyyy = today.getFullYear();
+        today = `${dd}/${mm}/${yyyy}`;
+
+        tableBody.innerHTML += `
+            <tr>
+                <td data-cell="#"> </td>
+                <td data-cell="Date">${today}</td>
+                <td data-cell="Title">${entryTitle}</td>
+                <td data-cell="Rating">${entryRating}</td>
+                <td data-cell="Rank #"> </td>
+            </tr>
+        `;
+
+        // Clear input fields after confirming
+        document.getElementById('entry-title').value = '';
+        document.getElementById('entry-rating').value = '';
+    }
+}
+
+
 
 
 
@@ -29,6 +63,7 @@ function onCancelClick(e) {
 function init() {
     addEntryButton.addEventListener('click', onAddEntryClick);
     cancelEntry.addEventListener('click', onCancelClick);
+    confirmEntry.addEventListener('click', onConfirmClick);
 }
 
 init();
